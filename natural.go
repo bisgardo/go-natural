eb41459f57b0a7d88f08c5dbde026fb4ecd664de
Michael Bisgaard Olesen <mbo@uber.com>
2017-10-06 11:14:11 +0200

    Extract helper function for readability

--- natural.go
+++ natural.go
@@ -10,63 +10,12 @@ func Natural(left, right string) int {
 		minLen = rightLen
 	}
 
-	idx := 0
-	for idx < minLen {
+	for idx := 0; idx < minLen; idx++ {
 		l := left[idx]
 		r := right[idx]
-		idx++
-		if l == r {
-			continue
+		if l != r {
+			return innerCompare(l, r, left, right, idx+1, minLen)
 		}
-
-		// Bytes l and r differ.
-
-		ln, lok := parseInt(l)
-		rn, rok := parseInt(r)
-
-		// Any number is larger than any non-number.
-		if !lok && rok {
-			return -1
-		}
-		if lok && !rok {
-			return 1
-		}
-
-		// Both bytes are numbers.
-		// Resolve and compare these numbers.
-		li := idx
-		for li < leftLen {
-			n, ok := parseInt(left[li])
-			if !ok {
-				break
-			}
-			li++
-			ln = 10*ln + n
-		}
-		ri := idx
-		for ri < rightLen {
-			n, ok := parseInt(right[ri])
-			if !ok {
-				break
-			}
-			ri++
-			rn = 10*rn + n
-		}
-
-		// Compare numbers.
-		if ln < rn {
-			return -1
-		}
-		if ln > rn {
-			return 1
-		}
-
-		// Numbers are equal, so one of them muast have leading zeros.
-		// Compare indices - higher is greater.
-		if li < ri {
-			return -1
-		}
-		return 1
 	}
 
 	// One string is a prefix of the other - longer is greater.
@@ -79,6 +28,59 @@ func Natural(left, right string) int {
 
 	// Strings are equal.
 	return 0
+}
+
+func innerCompare(l, r byte, left, right string, idx, minLen int) int {
+	// Bytes l and r are assumed to be different.
+	ln, lok := parseInt(l)
+	rn, rok := parseInt(r)
+
+	// Any number character is "larger" than any non-number one.
+	if !lok && rok {
+		return -1
+	}
+	if lok && !rok {
+		return 1
+	}
+
+	leftLen := len(left)
+	rightLen := len(right)
+
+	// Both bytes are numbers.
+	// Resolve and compare these numbers.
+	li := idx
+	for li < leftLen {
+		n, ok := parseInt(left[li])
+		if !ok {
+			break
+		}
+		li++
+		ln = 10*ln + n
+	}
+	ri := idx
+	for ri < rightLen {
+		n, ok := parseInt(right[ri])
+		if !ok {
+			break
+		}
+		ri++
+		rn = 10*rn + n
+	}
+
+	// Compare numbers.
+	if ln < rn {
+		return -1
+	}
+	if ln > rn {
+		return 1
+	}
+
+	// Numbers are equal, so one of them muast have leading zeros.
+	// Compare indices - higher is greater.
+	if li < ri {
+		return -1
+	}
+	return 1
 }
 
 func parseInt(b byte) (int, bool) {
